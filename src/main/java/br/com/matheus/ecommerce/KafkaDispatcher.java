6cda3240693dce7e56fd7a787cefdef299bb9134
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-03-02 18:29:39 -0300

    feat: added extract layer producer

---
+++ src/main/java/br/com/matheus/ecommerce/KafkaDispatcher.java
@@ -0,0 +1,48 @@
+package br.com.matheus.ecommerce;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.Closeable;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+class KafkaDispatcher implements Closeable {
+
+    private final KafkaProducer<String, String> producer;
+
+    public KafkaDispatcher() {
+        this.producer = new KafkaProducer<>(properties());
+    }
+
+    void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+        var record = new ProducerRecord<>(topic, key, value);
+        final Callback callback = (data, ex) -> {
+            if (ex != null) {
+                log.error(ex.getMessage());
+                return;
+            }
+            log.info("Success send topic: " + data.topic() + " - with partition: " + data.partition()
+                    + " - with offset: " + data.offset() + " - with timestamp: " + data.timestamp());
+        };
+        producer.send(record, callback).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+    @Override
+    public void close() {
+        producer.close();
+    }
+}
